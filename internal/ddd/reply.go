a9ed0713e9273ee64a374a927176ce43c74546f7
ALEX Huynh <htquangg@gmail.com>
2024-01-13 22:39:41 +0700

    feat(ddd): command & reply message

---
+++ internal/ddd/reply.go
@@ -0,0 +1,76 @@
+package ddd
+
+import (
+	"context"
+	"time"
+
+	"github.com/htquangg/microservices-poc/pkg/uid"
+)
+
+type (
+	Reply interface {
+		IDer
+		ReplyName() string
+		Payload() interface{}
+		Metadata() Metadata
+		OccurredAt() time.Time
+	}
+
+	ReplyHandler[T Reply] interface {
+		HandleReply(ctx context.Context, reply T) error
+	}
+
+	ReplyHandlerFunc[T Reply] func(ctx context.Context, reply T) error
+
+	ReplyOption interface {
+		configureReply(*reply)
+	}
+
+	reply struct {
+		Entity
+		payload    interface{}
+		metadata   Metadata
+		occurredAt time.Time
+	}
+)
+
+var _ Reply = (*reply)(nil)
+
+func NewReply(name string, payload interface{}, options ...ReplyOption) Reply {
+	return newReply(name, payload, options...)
+}
+
+func newReply(name string, payload interface{}, options ...ReplyOption) reply {
+	rep := reply{
+		Entity:     NewEntity(uid.GetManager().ID(), name),
+		payload:    payload,
+		metadata:   make(Metadata),
+		occurredAt: time.Now(),
+	}
+
+	for _, option := range options {
+		option.configureReply(&rep)
+	}
+
+	return rep
+}
+
+func (e reply) ReplyName() string {
+	return e.EntityName()
+}
+
+func (e reply) Payload() interface{} {
+	return e.payload
+}
+
+func (e reply) Metadata() Metadata {
+	return e.metadata
+}
+
+func (e reply) OccurredAt() time.Time {
+	return e.occurredAt
+}
+
+func (f ReplyHandlerFunc[T]) HandleReply(ctx context.Context, reply T) error {
+	return f(ctx, reply)
+}
